034ea45aed6a78c890905e295adb87517077ebf4
Kennysam90 <chiderasamson38@gmail.com>
2026-01-25 14:28:18 -0800

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import Zigzag from './Zigzag';
 import axios from 'axios';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
